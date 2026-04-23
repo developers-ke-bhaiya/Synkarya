efeb9af33cd6e0315c01caf511b5b9e3fbfa1399
Aditya-Bhaiya <adityamishra000@hotmail.com>
2026-04-23 19:54:46 +0530

    fix in app.jsx

--- synkarya/client/src/App.jsx
+++ synkarya/client/src/App.jsx
@@ -3,7 +3,7 @@ import { io } from "socket.io-client";
 import Auth from "./Auth";
 
 // 🔥 IMPORTANT: अपना Render backend URL डाल
-const socket = io("https://synkarya-backend.onrender.com");
+const socket = io("https://synkarya.onrender.com");
 
 let peer = null;
 let localStream = null;
